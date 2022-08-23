b1add27ca04267405476ca8f0e1ff7949f5e71df
Alex <alexsa-8@mail.ru>
2022-08-23 20:35:46 +0300

    Сделал 3 задания

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,74 @@
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-
-        System.out.println("Hello world!");
+    public static void printText() {
+        System.out.print("Введите интерисующий вас год:");
     }
+    public static void recordYear(int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " - високосный год");
+
+        } else {
+            System.out.println(year + " - не високосный год");
+        }
+    }
+    public static void printOS(){
+        System.out.println("Введите каким телефоном вы пользуетесь:");
+        System.out.println("если iOS - 0");
+        System.out.println("если Android - 1");
+    }
+    public static void printYear(){
+        System.out.println("Введите год выпуска вашего телефона: ");
+    }
+    public static void installOption(int clientOS, int clientDeviceYear, int yearOfRelease ){
+        //int clientOS = 0, clientDeviceYear = 0, yearOfRelease = 2015;
+        if (clientOS == 0 && clientDeviceYear < yearOfRelease) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOS == 0 && clientDeviceYear >= yearOfRelease) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear < yearOfRelease) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear >= yearOfRelease) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Ошибка");
+        }
+    }
+    public static void printDistance() {
+        System.out.print("Дистанция до клиента:");
+    }
+    public static void installTime(int e, int deliveryDistance){
+        //e=1+(deliveryDistance/40);
+        if (deliveryDistance<=20){
+            System.out.print("Потребуется дней: 1");
+        }
+        else if ((deliveryDistance-20)%40==0){
+            System.out.println("Потребуется дней: "+e);
+        }else if ((deliveryDistance-20)%40!=0){
+            System.out.println("Потребуется дней: "+(e+1));
+        }
+    }
+    public static void main(String[] args) {
+        //Задание 1
+        System.out.println("\nЗадание 1");
+        Scanner input=new Scanner(System.in);
+        printText();
+        int year=input.nextInt();
+        recordYear(year);
+        // Задание 2
+        System.out.println("\nЗадание 2");
+        int clientOS, clientDeviceYear,yearOfRelease=2015;
+        printOS();
+        clientOS=input.nextInt();
+        printYear();
+        clientDeviceYear=input.nextInt();
+        installOption(clientOS,clientDeviceYear,yearOfRelease);
+        // Задание 3
+        System.out.println("\nЗадание 3");
+        int deliveryDistance,e;
+        printDistance();
+        deliveryDistance=input.nextInt();
+        e=1+((deliveryDistance-20)/40);
+        installTime(e,deliveryDistance);
+    }
+
 }
